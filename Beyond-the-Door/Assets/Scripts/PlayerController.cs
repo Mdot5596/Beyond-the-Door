2073b6a4b0232942d80119144d81c42e72f53db4
Mdot5596 <morganhodge101010@gmail.com>
2025-04-04 15:24:48 +0100

    Initial Commit

---
+++ Beyond-the-Door/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get input from arrow keys or WASD
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Apply movement to the rigidbody
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Beyond-the-Door
 My Game Jam submission  https://itch.io/jam/mini-jame-gam-41
+
+
+
+## Assets Used
+
+Characters - https://zerie.itch.io/tiny-rpg-character-asset-pack
+Dungeon - https://pixel-poem.itch.io/dungeon-assetpuck 
+Room 1 ( Green / Outdoors ) - https://cainos.itch.io/pixel-art-top-down-basic
+Room 2 ( Blue / WATER) - https://snowhex.itch.io/dungeon-gathering
